83c8ed958bf48de6607fac05f116ff127188bc27
Adamski B <aboulton@live.co.uk>
2020-06-17 21:17:49 +0100

    added message history

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
+
+var messages = [
+    {name:"Tim", message:"Hi"},
+    {name:"Bob", message:"Hello"}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+});
+
 var server = app.listen(5001, () => {
     console.log('server is listening on port ', server.address().port)
 });
